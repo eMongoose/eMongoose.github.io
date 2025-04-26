64c1373104c278db2d370b72670c389033e2c8c2
Emily <eha80@sfu.ca>
2025-04-26 00:51:22 -0700

    fixing landing page

--- my-app/app/routes/home.tsx
+++ my-app/app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../aboutme/aboutme";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "eMongoose's GitHub Pages" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
